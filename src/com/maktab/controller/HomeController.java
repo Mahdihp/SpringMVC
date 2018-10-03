77f93e429bf4239c7c7bf07f562612154cefedbc
mahdi <mahdihp.devsc@gmail.com>
2018-10-03 15:42:12 +0330

    Add Service & Repository Layer

--- src/com/maktab/controller/HomeController.java
+++ src/com/maktab/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.maktab.controller;
 
+import com.maktab.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,9 +10,21 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(path = "/person")
 public class HomeController {
 
-    @RequestMapping(path = "/test")
+
+    private final PersonService personService;
+
+    @Autowired
+    public HomeController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @RequestMapping(path = "/show")
+    public ModelAndView showForm() {
+        return new ModelAndView("show-person");
+    }
+
+    @RequestMapping(path = "/")
     public ModelAndView homeMethod() {
-        System.out.println("Test Controller");
         return new ModelAndView("index");
     }
 }
--- src/com/maktab/dao/PersonRepository.java
+++ src/com/maktab/dao/PersonRepository.java
@@ -1,6 +1,8 @@
 package com.maktab.dao;
 
 import com.maktab.model.Person;
+import com.maktab.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +15,9 @@ import java.util.Map;
 public class PersonRepository {
 
 
+
     private static Map<Long, Person> personMap = new HashMap<>();
+
 
     public void create(Person person) {
         personMap.put(person.getId(), person);
