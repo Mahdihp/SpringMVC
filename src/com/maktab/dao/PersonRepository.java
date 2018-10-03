a0698255cc07bb5736a5bd46fd029f32727ee809
mahdi <mahdihp.devsc@gmail.com>
2018-10-03 15:35:25 +0330

    Add Service & Repository Layer

---
+++ src/com/maktab/dao/PersonRepository.java
@@ -0,0 +1,25 @@
+package com.maktab.dao;
+
+import com.maktab.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class PersonRepository {
+
+
+    private static Map<Long, Person> personMap = new HashMap<>();
+
+    public void create(Person person) {
+        personMap.put(person.getId(), person);
+    }
+
+    public List<Person> readAll() {
+        return new ArrayList<>(personMap.values());
+    }
+}
---
+++ src/com/maktab/model/Person.java
@@ -0,0 +1,24 @@
+package com.maktab.model;
+
+public class Person {
+
+    private Long id;
+    private String fullName;
+    public Person(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
